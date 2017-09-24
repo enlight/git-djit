c8a92503fba8ed01b3f49b2e9839fffa352e8e67
Vadim Macagon <vadim.macagon@gmail.com>
2017-09-24 22:33:15 +0700

    Add a comment for MenuItemId enum

--- src/common/menu-item-ids.ts
+++ src/common/menu-item-ids.ts
@@ -1,6 +1,7 @@
 // Copyright (c) 2017 Vadim Macagon
 // MIT License, see LICENSE file for full terms.
 
+/** Identifiers for App/Window Menu items. */
 enum MenuItemId {
   AddLocalRepository = 'add-local-repo'
 }
